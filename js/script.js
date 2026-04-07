98fb2b377d9442768d02020ce5204019ae78668d
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-04-07 17:53:55 +0200

    feat: hide input form and show matched results with success and error styles
    feat: nasconde il form e mostra risultati corretti ed errati con stili dinamici

--- js/script.js
+++ js/script.js
@@ -23,44 +23,60 @@
 const input = document.querySelector('#answers-form');
 const numeriRandom = [];
 for (i = 0; i < 5; i++) {
-    let numeroRandom = Math.floor(Math.random()*50) + 1;
+    let numeroRandom = Math.floor(Math.random() * 50) + 1;
     numeriRandom.push(numeroRandom);
     console.log(numeriRandom);
 }
-const numberi = document.querySelector('#numbers-list');
+const numeri = document.querySelector('#numbers-list');
 let html = '';
 
 for (let i = 0; i < numeriRandom.length; i++) {
     html += `<li class="badge text-bg-dark">${numeriRandom[i]}</li>`;
 }
-numberi.innerHTML = html;
+numeri.innerHTML = html;
 
 
-let countDown = 31;
+let countDown = 30;
 const countDownShow = document.querySelector('#countdown');
-const timer = ()=>{
-    countDown --;
-    if(countDown>=0){
+const timer = () => {
+    countDown--;
+    if (countDown >= 0) {
         console.log(countDown);
         countDownShow.innerHTML = `${countDown}`;
-    }else {
+    } else {
         countDownShow.classList.add('d-none');
-        numberi.classList.add('d-none');
+        numeri.classList.add('d-none');
         input.classList.remove('d-none');
         clearInterval(interval);
-
-        }
     }
-const interval = setInterval(timer , 1000);
+}
+const interval = setInterval(timer, 1000);
 
-const numbersOfInput=document.querySelectorAll('#input-group')
+const numbersOfInput = document.querySelectorAll('.form-control')
 
 const button = document.querySelector('#btn');
 
-button.addEventListener("click",()=>{
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector('#instructions').classList.add('d-none');
+    
+
     const userNumbers = [];
-    numbersOfInput.forEach(input => {
-        const value = Number(input.value);
-        userNumbers.push(value);
+    numbersOfInput.forEach(input2 => {
+        const val = Number(input2.value);
+        userNumbers.push(val);
     });
+
+    input.classList.add('d-none');
+    
+    let html2 = '';
+    for (let i = 0; i < userNumbers.length; i++) {
+        if (numeriRandom.includes(userNumbers[i])) {
+            html2 += `<li class="badge text-bg-success">${userNumbers[i]}</li>`;
+        } else {
+            html2 += `<li class="badge text-bg-danger">${userNumbers[i]}</li>`;
+        }
+    } numeri.innerHTML = html2;
+    numeri.classList.remove('d-none');
 });
+
